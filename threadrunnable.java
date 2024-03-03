c22e3239ebb2c3fe93cba822f9204c30206b8544
appuharsha17 <157977669+appuharsha17@users.noreply.github.com>
2024-03-03 19:39:42 +0530

    Add files via upload

---
+++ threadrunnable.java
@@ -0,0 +1,27 @@
+package javop;
+
+class MyRunnable implements Runnable {
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " is starting.");
+            Thread.sleep(500); // Suspend the thread for 500 milliseconds
+            System.out.println(Thread.currentThread().getName() + " is finished.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class threadrunnable {
+    public static void main(String[] args) {
+        // Create multiple threads using MyRunnable
+        Thread ob1 = new Thread(new MyRunnable(), "Thread 1");
+        Thread ob2 = new Thread(new MyRunnable(), "Thread 2");
+        Thread ob3 = new Thread(new MyRunnable(), "Thread 3");
+
+        // Start each thread
+        ob1.start();
+        ob2.start();
+        ob3.start();
+    }
+}
